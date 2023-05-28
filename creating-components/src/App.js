bcc79a7494baf9babbcda6d8de50079bdd814573
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-05-27 19:14:18 -0500

    updated UI

--- creating-components/src/App.js
+++ creating-components/src/App.js
@@ -12,7 +12,7 @@ function NavBar(){
     <>
       <div className='bg-white h-16 shadow-md text-3xl p-3 '>
         <button className="float-left">☰</button>
-        <button className="float-right"><span className="text-2xl">Guest</span>☺</button>
+        <button className="float-right"><span className="text-2xl">Катталуу</span>☺</button>
       </div>
     </>
   )
