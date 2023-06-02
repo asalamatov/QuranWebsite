193ee0d76c3acb18324283a0ab4fbf0ec04573d2
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-06-01 20:00:45 -0500

    updated list rendering

--- creating-components/src/components/SurahsList.js
+++ creating-components/src/components/SurahsList.js
@@ -6,6 +6,7 @@ export default function SurahsList() {
     }
     const a_list = list()
     return (
+      <>
         <div className='w-full max-w-7xl my-0 mx-auto p-5 '>
           <div className=' rounded py-0 px-0 shadow-md bg-white' style={{color: '#00bcd4'}}>
             <div className="text-center py-6 px-0">
@@ -21,7 +22,27 @@ export default function SurahsList() {
                     </a>
                 </div>
             </div>
+            
           </div>
         </div>
+        <div className='w-full max-w-7xl my-0 mx-auto p-5 '>
+          <div className=' rounded py-0 px-0 shadow-md bg-white' style={{color: '#00bcd4'}}>
+            <div className="text-center py-6 px-0">
+                <div className="text-xl font-bold">
+                    <a href="#" className="py-6 flex-row">
+                        <div class="flex- font-bold w-10 text-center inline-block">1</div>
+                        <div class="inline-block">
+                            <div>Китепти Ачуучу</div>
+                        </div>
+                        <div>
+                            الفاتحة
+                        </div>
+                    </a>
+                </div>
+            </div>
+            
+          </div>
+        </div>
+      </>
     )
   }
